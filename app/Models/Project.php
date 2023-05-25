1c4166bb5cc3b19b322ecdf41bf00ded6477bf7c
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-05-25 16:00:39 +0200

    Mutator path immagine

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -21,5 +22,11 @@ class Project extends Model
 
     public function technologies(){
         return $this->belongsToMany(Technology::class)->withTimestamps();
+    }
+
+    protected function immagine(): Attribute {
+        return Attribute::make(
+            get: fn(string|null $value) => $value ? asset('storage/' . $value) : null,
+        );
     }
 }
