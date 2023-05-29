3379ebc12fc548bb0d00ed4ee6cd1da1b6c40a9a
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-05-29 15:00:21 +0200

    Creazione rotte Technologies e Types

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TechnologyController;
+use App\Http\Controllers\Api\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +21,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Rotte API per la lista e il dettaglio dei progetti
 Route::get('projects', [ProjectController::class, 'index']);
 Route::get('projects/{slug}', [ProjectController::class, 'show']);
+
+// Rotte API per la lista e il dettaglio delle tecnologie
+Route::get('technologies', [TechnologyController::class. 'index']);
+Route::get('techonolgies/{slug}', [TechnologyController::class, 'show']);
+
+// Rotta API per la lista e il dettaglio della tipologia
+Route::get('types', [TypeController::class, 'index']);
+Route::get('types/{slug}', [TypeController::class, 'show']);
+
