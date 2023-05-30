b1bfec5608b4a9c1a1b5c1bfff10d5eb2f2f6a6d
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-05-30 17:22:57 +0200

    Metodo che manda le mail all'invio del form

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\NewLead;
 use App\Models\Lead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class LeadController extends Controller
 {
@@ -20,8 +22,9 @@ class LeadController extends Controller
         $lead->nome = $data['name'];
         $lead->email = $data['email'];
         $lead->messaggio = $data['messagge'];
+
         $lead->save();
 
-        
+        Mail::to($lead->email)->send(new NewLead($lead));
     }
 }
