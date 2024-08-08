ed5befe2ce3056a06b7056c1119f45c649d8e7f9
Luca Zanfrisco <lzanfrisco9@gmail.com>
2024-08-08 15:09:20 +0200

    new dashboard layout

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -14,11 +14,13 @@ class LeadController extends Controller
 
         $data = $request->validate([
             'name' => 'required|string',
+            'surname' => 'required|string',
             'email' => 'required|email',
             'messagge' => 'nullable|string'
         ]);
 
         $lead =  new Lead();
+        $lead->surname = $data['surname'];
         $lead->nome = $data['name'];
         $lead->email = $data['email'];
         $lead->messaggio = $data['messagge'];
