2144d9097d3142ba9386a4b8f657cbf2416464ea
Luca Zanfrisco <lzanfrisco9@gmail.com>
2024-11-06 22:01:35 +0100

    aggiunte informazioni dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Lead;
+use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index (){
-        return view('admin.dashboard');
+        $listaProgetti = Project::all();
+        $listaTechnologie = Technology::all();
+        $listaTipologie = Type::all();
+        $listaContatti = Lead::all();
+        $numeroContatti = $listaContatti ? count($listaContatti) : 0;
+        $numeroTipologie = $listaTipologie ? count($listaTipologie) : 0;
+        $numeroProgetti = $listaProgetti ? count($listaProgetti) : 0;
+        $numeroTecnologie = $listaTechnologie ? count($listaTechnologie) : 0;
+
+        $data = [
+            "numeroProgetti" => $numeroProgetti,
+            "numeroTecnologie" => $numeroTecnologie,
+            "numeroTipologie" => $numeroTipologie,
+            "numeroContatti" => $numeroContatti
+        ];
+        return view('admin.dashboard', compact("data"));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="ms-5">
     <h2 class="fs-4 text-secondary my-4">
         {{ __('Dashboard') }}
     </h2>
+    <h6>Sezione dell'applicaizone per una visualizzazione generale di quello che è stato salvato nel database dei propri progetti</h6>
+    <div class="row row-cols-5 justify-content-center">
+        <div class="col">
+            <a class="text-decoration-none text-black" href="{{ route("admin.project.index") }}">
+                <div class="card text-center py-2">
+                    <div>Numero di progetti</div>
+                    <div class="card-body">
+                        {{ $data["numeroProgetti"] }}
+                    </div>
+                </div> 
+            </a>
+        </div>
+        <div class="col">
+            <a class="text-decoration-none text-black" href="{{ route("admin.technologies.index") }}">
+                <div class="card text-center py-2">
+                    <div>Numero di tecnologie</div>
+                    <div class="card-body">
+                        {{ $data["numeroTecnologie"] }}
+                    </div>
+                </div> 
+            </a>
+        </div>
+        <div class="col">
+            <a class="text-decoration-none text-black" href="{{ route("admin.types.index") }}">
+                <div class="card text-center py-2">
+                    <div>Numero di tipologie</div>
+                    <div class="card-body">
+                        {{ $data["numeroTipologie"] }}
+                    </div>
+                </div> 
+            </a>
+        </div>
+        <div class="col">
+            <a class="text-decoration-none text-black" href="{{ route("admin.leads.index") }}">
+                <div class="card text-center py-2">
+                    <div>Numero di Contatti</div>
+                    <div class="card-body">
+                        {{ $data["numeroContatti"] }}
+                    </div>
+                </div> 
+            </a>
+        </div>
+    </div>
+
 </div>
 @endsection
